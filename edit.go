ff3f89cd028949eaf0e645aa5b7ef2b173d4c500
Stefan Haller <stefan@haller-berlin.de>
2026-01-03 12:27:28 +0100

    Add ctrl-b/ctrl-f for left/right navigation in editor
    This was added to lazygit in https://github.com/jesseduffield/lazygit/pull/2104,
    and it was forgotten to add to gocui in faf0bad61195.

--- edit.go
+++ edit.go
@@ -38,11 +38,11 @@ func SimpleEditor(v *View, key Key, ch rune, mod Modifier) bool {
 		v.TextArea.MoveCursorUp()
 	case (key == KeyArrowLeft || ch == 'b') && (mod&ModAlt) != 0:
 		v.TextArea.MoveLeftWord()
-	case key == KeyArrowLeft:
+	case key == KeyArrowLeft || key == KeyCtrlB:
 		v.TextArea.MoveCursorLeft()
 	case (key == KeyArrowRight || ch == 'f') && (mod&ModAlt) != 0:
 		v.TextArea.MoveRightWord()
-	case key == KeyArrowRight:
+	case key == KeyArrowRight || key == KeyCtrlF:
 		v.TextArea.MoveCursorRight()
 	case key == KeyEnter:
 		v.TextArea.TypeCharacter("\n")
